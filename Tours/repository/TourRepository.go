7135eaf2636123bc7a9c79ccb7efff87d92d8476
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2024-03-19 17:38:20 +0100

    fix: Go tour repo

--- Tours/repository/TourRepository.go
+++ Tours/repository/TourRepository.go
@@ -11,14 +11,6 @@ type TourRepository struct {
 }
 
 func (repo *TourRepository) CreateTour(tour *model.Tour) (*model.Tour, error) {
-	var maxID int
-	if err := repo.DatabaseConnection.Table(`tours."Tour"`).Select("COALESCE(MAX(\"Id\"), 0)").Row().Scan(&maxID); err != nil {
-		return nil, err
-	}
-
-	tour.ID = maxID + 1
-
-	println(tour.ID)
 	dbResult := repo.DatabaseConnection.Table(`tours."Tour"`).Create(tour)
 
 	if dbResult.Error != nil {
