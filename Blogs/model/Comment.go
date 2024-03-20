20bc772879d55e705871cc7eae3268b2c412c591
Nedeljko Babic <bnedeljko0@gmail.com>
2024-03-20 11:31:32 +0100

    fix: struct change

--- Blogs/model/Comment.go
+++ Blogs/model/Comment.go
@@ -7,6 +7,8 @@ import (
 type Comment struct {
 	Id           int       `json:"id" gorm:"column:Id;primaryKey;autoIncrement"`
 	UserId       int       `json:"userId" gorm:"column:UserId"`
+	Username     string    `json:"username" gorm:"-"`
+	ProfilePic   string    `json:"profilePic" gorm:"-"`
 	CreationDate time.Time `json:"creationDate" gorm:"column:CreationDate"`
 	Description  string    `json:"description" gorm:"column:Description"`
 	LastEditDate time.Time `json:"lastEditDate" gorm:"column:LastEditDate"`
