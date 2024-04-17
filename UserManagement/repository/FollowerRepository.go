f8a9868466d5d22ad78b4e6cd00a4ff226f92ee9
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2024-04-17 22:29:20 +0200

    feat: connecting neo4j with user_management_service

--- UserManagement/repository/FollowerRepository.go
+++ UserManagement/repository/FollowerRepository.go
@@ -1,9 +1,50 @@
 package repository
 
 import (
+	"context"
+	"log"
+	"os"
+
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 type FollowerRepository struct {
-	FollowerDriver *neo4j.DriverWithContext
+	driver neo4j.DriverWithContext
+	logger *log.Logger
+}
+
+func NewFollowerRepository(logger *log.Logger) (*FollowerRepository, error) {
+	uri := os.Getenv("NEO4J_DB")
+	user := os.Getenv("NEO4J_USERNAME")
+	pass := os.Getenv("NEO4J_PASS")
+	auth := neo4j.BasicAuth(user, pass, "")
+
+	print("--------------------")
+	print(user, pass)
+
+	driver, err := neo4j.NewDriverWithContext(uri, auth)
+	if err != nil {
+		logger.Panic(err)
+		return nil, err
+	}
+
+	return &FollowerRepository{
+		driver: driver,
+		logger: logger,
+	}, nil
+}
+
+func (mr *FollowerRepository) CheckConnection() {
+	ctx := context.Background()
+	err := mr.driver.VerifyConnectivity(ctx)
+	if err != nil {
+		mr.logger.Panic(err)
+		return
+	}
+
+	mr.logger.Printf(`Neo4J server address: %s`, mr.driver.Target().Host)
+}
+
+func (mr *FollowerRepository) CloseDriverConnection(ctx context.Context) {
+	mr.driver.Close(ctx)
 }
