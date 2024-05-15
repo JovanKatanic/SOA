860199940600ca9d5f3d5b92e27cd4c20c9eda91
JovanSarac <jovan_bn@outlook.com>
2024-05-15 18:14:14 +0200

    fix:Change RatingSum on Ratings[]

--- Blogs/handl/BlogHandler.go
+++ Blogs/handl/BlogHandler.go
@@ -16,17 +16,27 @@ type BlogHandler struct {
 
 func (h BlogHandler) GetBlog(ctx context.Context, request *blogs.GetBlogRequest) (*blogs.GetBlogResponse, error) {
 	var blog model.BlogPage
-	fmt.Println("Usao u handler")
 	if err := h.DatabaseConnection.Table(`blog."Blogs"`).Where(`"Blogs"."Id" = ?`, request.Id).First(&blog).Error; err != nil {
 		return nil, err
 	}
 
+	ratingList := make([]*blogs.Rating, len(blog.Ratings))
+	for i, rating := range blog.Ratings {
+		ratingList[i] = &blogs.Rating{
+			UserId:       int32(rating.UserId),
+			CreationDate: rating.CreationDate.String(),
+			RatingValue:  int32(rating.RatingValue),
+		}
+	}
+
 	blogic := &blogs.Blog{
-		Id:          1,
-		Title:       blog.Title,
-		Description: blog.Description,
-		Status:      1,
-		UserId:      1,
+		Id:           int32(blog.Id),
+		Title:        blog.Title,
+		Description:  blog.Description,
+		CreationDate: blog.CreationDate.String(),
+		Status:       int32(blog.Status),
+		UserId:       int32(blog.UserId),
+		RatingSum:    ratingList,
 	}
 
 	fmt.Println(blog)
