e2bdc6038fa02ff47d843a871e643c2ac10cce26
JovanKatanic <jovan.katanic0101@gmail.com>
2024-05-15 03:26:06 +0200

    feat: added login without grpc stuff

--- Stakeholders/go.mod
+++ Stakeholders/go.mod
@@ -18,6 +18,8 @@ require (
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/google/uuid v1.6.0
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- Stakeholders/handl/AuthHandler.go
+++ Stakeholders/handl/AuthHandler.go
@@ -7,6 +7,10 @@ import (
 	"stakeholders_service/model"
 	"stakeholders_service/proto/auth"
 
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -15,12 +19,50 @@ type AuthHandler struct {
 	DatabaseConnection *gorm.DB
 }
 
+func ConvertToString(role int) string {
+	switch role {
+	case 0:
+		return "administrator"
+	case 1:
+		return "author"
+	case 2:
+		return "tourist"
+	default:
+		return "unknown"
+	}
+}
+
 func (h AuthHandler) Greet(ctx context.Context, request *auth.Request) (*auth.Response, error) {
-	var users []model.User
-	if err := h.DatabaseConnection.Table(`stakeholders."Users"`).Find(&users).Error; err != nil {
+	var user model.User
+	if err := h.DatabaseConnection.Table(`stakeholders."Users"`).Where(`"Users"."Username" = ? and "Users"."IsActive" = true`, "stefanstojanovic").First(&user).Error; err != nil {
 		return nil, err
 	}
-	fmt.Println(users)
+
+	var person model.Person
+	if err := h.DatabaseConnection.Table(`stakeholders."People"`).Where(`"People"."UserId" = ?`, user.ID).First(&person).Error; err != nil {
+		return nil, err
+	}
+
+	claims := jwt.MapClaims{
+		"jti":      uuid.New().String(),
+		"id":       user.ID,
+		"username": user.Username,
+		"personId": person.ID,
+		"role":     ConvertToString(user.Role),
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte("your_secret_key")) // Replace "your_secret_key" with your actual secret key
+	if err != nil {
+		return nil, err
+	}
+	authenticationResponse := model.AuthenticationTokensDto{
+		ID:          user.ID,
+		AccessToken: tokenString,
+	}
+
+	fmt.Println(authenticationResponse)
+
 	return &auth.Response{
 		Greeting: fmt.Sprintf("Hi %s!", request.Name),
 	}, nil
